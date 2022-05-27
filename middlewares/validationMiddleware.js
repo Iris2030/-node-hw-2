711798fcfeef5def25220dc78f9c27ce74913950
Iris2030 <ibazhinova@gmail.com>
2022-05-27 17:22:06 +0200

    new

--- middlewares/validationMiddleware.js
+++ middlewares/validationMiddleware.js
@@ -7,8 +7,7 @@ module.exports={
             name: Joi.string()
               .min(3)
               .max(30)
-              .required()
-              .pattern(/^[a-zA-Zа-яА-ЯёЁ\s]+$/),
+              .required(),
             email: Joi.string().email({
               minDomainSegments: 2,
               tlds: { allow: true },
